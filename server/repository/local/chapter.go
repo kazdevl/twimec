940bd84f1a68ec1d60e5ce683d1cf9bcffecf6fd
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 11:03:23 +0900

    feat(repository): impl chapter repository

--- server/repository/local/chapter.go
+++ server/repository/local/chapter.go
@@ -1,23 +1,59 @@
 package local
 
-import "github.com/kazdevl/twimec/domain"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kazdevl/twimec/domain"
+)
 
 type ChapterRepository struct {
-	StorageLocation string
+	storagePath string
 }
 
-func NewChapterRepository(sl string) *ChapterRepository {
+func NewChapterRepository(sp string) *ChapterRepository {
 	return &ChapterRepository{
-		StorageLocation: sl,
+		storagePath: sp,
 	}
 }
 
 func (c *ChapterRepository) GetList(contentID string) []domain.Chapter {
-	// TODO impl
-	return nil
+	contentDir := filepath.Join(c.storagePath, contentID)
+	des, err := os.ReadDir(contentDir)
+	if err != nil {
+		return nil
+	}
+	result := make([]domain.Chapter, len(des))
+	for index, de := range des {
+		pagesByte, err := os.ReadFile(filepath.Join(contentDir, de.Name()))
+		if err != nil {
+			return nil
+		}
+		pages := string(pagesByte)
+		pagesSlice := strings.Split(pages, ",")
+		result[index] = domain.Chapter{
+			ContentID: contentID,
+			Index:     index,
+			Icon:      pagesSlice[0],
+			Pages:     string(pages),
+		}
+	}
+	return result
 }
 
 func (c *ChapterRepository) Store(chapter domain.Chapter) error {
-	// TODO impl
+	contentDir := filepath.Join(c.storagePath, chapter.ContentID)
+	des, err := os.ReadDir(contentDir)
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(fmt.Sprintf("%s/%d.txt", contentDir, len(des)))
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+	f.WriteString(chapter.Pages)
 	return nil
 }
