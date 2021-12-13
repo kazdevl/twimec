aacf1ecb1f733affca0d6663eab21bb91cb962a9
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-14 00:17:10 +0900

    feat(web): create sturct for render template

--- server/web/web.go
+++ server/web/web.go
@@ -1,15 +1,38 @@
 package web
 
-import "github.com/labstack/echo/v4"
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"path/filepath"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func NewTemplate() *Template {
+	templateDir, _ := filepath.Abs("./template")
+	return &Template{
+		templates: template.Must(template.ParseGlob(fmt.Sprintf("%s/*.html", templateDir))),
+	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func ContentListPage(c echo.Context) error {
-	return nil
+	return c.Render(http.StatusOK, "contentList", "World")
 }
 
 func ContentPage(c echo.Context) error {
-	return nil
+	return c.Render(http.StatusOK, "content", "World")
 }
 
 func ChapterPage(c echo.Context) error {
-	return nil
+	return c.Render(http.StatusOK, "chapter", "World")
 }
