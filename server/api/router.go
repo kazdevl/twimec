162bb9cd89b04e1efa90f0d6c14f456f55af80ca
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 22:34:26 +0900

    feat(api): serve html

--- server/api/router.go
+++ server/api/router.go
@@ -2,10 +2,17 @@ package api
 
 import (
 	"github.com/kazdevl/twimec/usecase"
+	"github.com/kazdevl/twimec/web"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e *echo.Echo) {
+	// web server
+	e.GET("/", web.ContentListPage)
+	e.GET("/:content_id", web.ContentPage)
+	e.GET("/:content_id/:chapter_number", web.ChapterPage)
+
+	// TODO impl
 	e.POST("/config/auth", usecase.RegisterCredential)
 	e.POST("/config/contents", usecase.RegisterContent)
 	e.PUT("/config/contents/:user_name", usecase.UpdateContent)
