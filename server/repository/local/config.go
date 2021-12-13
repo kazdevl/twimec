09e472a6dcf3730642a97a60d0c07b7ab1b82e6e
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 12:28:27 +0900

    feat(repository): impl config repository

--- server/repository/local/config.go
+++ server/repository/local/config.go
@@ -1,30 +1,85 @@
 package local
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/kazdevl/twimec/domain"
 )
 
 type ConfigRepository struct {
-	StorageLocation string
+	storagePath string
 }
 
-func NewConfigRepository(sl string) *ConfigRepository {
+func NewConfigRepository(sp string) *ConfigRepository {
 	return &ConfigRepository{
-		StorageLocation: sl,
+		storagePath: sp,
 	}
 }
 
+func (c *ConfigRepository) Get(contentID string) *domain.ConfigContentAcquisition {
+	contentDir := filepath.Join(c.storagePath, contentID)
+	f, err := os.Open(fmt.Sprintf("%s/acquisition.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return nil
+	}
+	var config domain.ConfigContentAcquisition
+	if err := json.NewDecoder(f).Decode(&config); err != nil {
+		return nil
+	}
+	return &config
+}
+
 func (c *ConfigRepository) All() []domain.ConfigContentAcquisition {
-	// TODO impl
-	return nil
+	des, err := os.ReadDir(c.storagePath)
+	if err != nil {
+		return nil
+	}
+	result := make([]domain.ConfigContentAcquisition, len(des))
+	for index, de := range des {
+		contentDir := filepath.Join(c.storagePath, de.Name())
+		f, err := os.Open(fmt.Sprintf("%s/acquisition.json", contentDir))
+		defer f.Close()
+		if err != nil {
+			return nil
+		}
+		var config domain.ConfigContentAcquisition
+		if err := json.NewDecoder(f).Decode(&config); err != nil {
+			return nil
+		}
+
+		result[index] = config
+	}
+	return result
 }
 
 func (c *ConfigRepository) Store(config domain.ConfigContentAcquisition) error {
-	// TODO impl
-	return nil
+	contentDir := filepath.Join(c.storagePath, config.ContentID)
+	if err := os.Mkdir(contentDir, 0777); err != nil {
+		return err
+	}
+
+	f, err := os.Create(fmt.Sprintf("%s/acquisition.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = json.NewEncoder(f).Encode(config)
+	return err
 }
 
 func (c *ConfigRepository) Update(config domain.ConfigContentAcquisition) error {
-	// TODO impl
-	return nil
+	contentDir := filepath.Join(c.storagePath, config.ContentID)
+	f, err := os.Create(fmt.Sprintf("%s/acquisition.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = json.NewEncoder(f).Encode(config)
+	return err
 }
