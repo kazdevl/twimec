b884338801a5141af0215773bb747330c8912ead
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 18:30:31 +0900

    feat(cmd): impl startCmd proccess

--- server/cmd/start/start.go
+++ server/cmd/start/start.go
@@ -1,6 +1,18 @@
 package start
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/go-co-op/gocron"
+	"github.com/kazdevl/twimec/api"
+	"github.com/kazdevl/twimec/repository/local"
+	"github.com/kazdevl/twimec/usecase"
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 )
 
@@ -17,5 +29,33 @@ func NewCmd() *cobra.Command {
 }
 
 func proccess(token string) {
-	// TODO impl
+	// 1. launch api server
+	e := echo.New()
+	api.RegisterRoutes(e)
+	go func() {
+		if err := e.Start(":6666"); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// 2. call cron job
+	tclient := usecase.NewTClient(false, token)
+	homeDir, _ := os.UserHomeDir()
+	chapterRepo := local.NewChapterRepository(fmt.Sprintf("%s/twimec/storage/contents", homeDir))
+	configRepo := local.NewConfigRepository(fmt.Sprintf("%s/twimec/storage/config/contents", homeDir))
+	cronjob := usecase.NewCronjob(tclient, chapterRepo, configRepo)
+
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(1).Day().At("21:00").Do(cronjob.FetchContents)
+	s.StartAsync()
+
+	// 3. Graceful shatdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
