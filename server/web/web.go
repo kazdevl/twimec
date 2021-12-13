1043503bb8c2b808eda34b58593af213077b6363
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 22:34:06 +0900

    feat(web): init

---
+++ server/web/web.go
@@ -0,0 +1,15 @@
+package web
+
+import "github.com/labstack/echo/v4"
+
+func ContentListPage(c echo.Context) error {
+	return nil
+}
+
+func ContentPage(c echo.Context) error {
+	return nil
+}
+
+func ChapterPage(c echo.Context) error {
+	return nil
+}
