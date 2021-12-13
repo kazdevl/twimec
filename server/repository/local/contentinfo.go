e5f732cb4807c7444c8a88f7f0b72af070c09b51
kazdevl <39262724+kazdevl@users.noreply.github.com>
2021-12-13 12:46:52 +0900

    feat(repository): impl contentinfo repository

--- server/repository/local/contentinfo.go
+++ server/repository/local/contentinfo.go
@@ -1,33 +1,81 @@
 package local
 
-import "github.com/kazdevl/twimec/domain"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/kazdevl/twimec/domain"
+)
 
 type ContentInfoRepository struct {
-	StorageLocation string
+	storagePath string
 }
 
-func NewContentInfoRepository(sl string) *ContentInfoRepository {
+func NewContentInfoRepository(sp string) *ContentInfoRepository {
 	return &ContentInfoRepository{
-		StorageLocation: sl,
+		storagePath: sp,
 	}
 }
 
 func (c *ContentInfoRepository) GetAuthorName(contentID string) string {
-	// TODO impl
-	return ""
+	contentDir := filepath.Join(c.storagePath, contentID)
+	f, err := os.Open(fmt.Sprintf("%s/info.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return ""
+	}
+	var config domain.ContentInfo
+	if err := json.NewDecoder(f).Decode(&config); err != nil {
+		return ""
+	}
+	return config.AuthorName
 }
 
 func (c *ContentInfoRepository) All() []domain.ContentInfo {
-	// TODO impl
-	return nil
+	des, err := os.ReadDir(c.storagePath)
+	if err != nil {
+		return nil
+	}
+	result := make([]domain.ContentInfo, len(des))
+	for index, de := range des {
+		contentDir := filepath.Join(c.storagePath, de.Name())
+		f, err := os.Open(fmt.Sprintf("%s/info.json", contentDir))
+		defer f.Close()
+		if err != nil {
+			return nil
+		}
+		var info domain.ContentInfo
+		if err := json.NewDecoder(f).Decode(&info); err != nil {
+			return nil
+		}
+
+		result[index] = info
+	}
+	return result
 }
 
 func (c *ContentInfoRepository) Store(content domain.ContentInfo) error {
-	// TODO impl
-	return nil
+	contentDir := filepath.Join(c.storagePath, content.ID)
+	f, err := os.Create(fmt.Sprintf("%s/info.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = json.NewEncoder(f).Encode(content)
+	return err
 }
 
 func (c *ContentInfoRepository) Update(content domain.ContentInfo) error {
-	// TODO impl
-	return nil
+	contentDir := filepath.Join(c.storagePath, content.ID)
+	f, err := os.Create(fmt.Sprintf("%s/info.json", contentDir))
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	err = json.NewEncoder(f).Encode(content)
+	return err
 }
